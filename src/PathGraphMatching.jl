95bf24b001741820b9a9ad016afc1d40267a25b6
Ken Alan Berkpinar <ken.berkpinar@gmail.com>
2026-08-03 15:11:49 +0200

    fix wrong case distinction

--- src/PathGraphMatching.jl
+++ src/PathGraphMatching.jl
@@ -88,9 +88,8 @@ module pathGraphMatching
         λ = 0.0
 
         # redefine f0, f1 and fλ depending on whether the QAP should be solved or not, s.t. f0 is always convex and f1 is always concave.
-        if !solveQAP
-            fλNormalizedFinal = fλNormalized
-        else
+        fλNormalizedFinal = fλNormalized
+        if solveQAP
             fλNormalizedFinal = fλ_QAP
         end
 
